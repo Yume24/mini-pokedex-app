af310ab8041e34c58a4c556627e8f9d2d778a3fc
Yume24 <antonijaros04@gmail.com>
2025-07-19 17:19:58 +0200

    change error message style

--- src/routes/home/PokemonListError.tsx
+++ src/routes/home/PokemonListError.tsx
@@ -1,3 +1,3 @@
 export default function PokemonListError() {
-    return <p className="text-red-500 font-bold m-auto text-center">Error loading Pokemon</p>
+    return <h2 className="text-error-content font-bold my-5 m-auto text-center text-2xl">Error loading Pokemon</h2>
 }
